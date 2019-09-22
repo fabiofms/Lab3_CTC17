daff5d1548923372e0663adf2dd2c34c96bafd0f
fabiomoreirafms <36686895+fabiomoreirafms@users.noreply.github.com>
2019-09-22 09:30:28 -0300

    Update script.py
    changed max tree depth to 10 on script

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ y = examples['classification']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Classifier.get_predictor(train_set, attributes, pattern, min_gain, max_tree_depth)
-predictor = Classifier.get_predictor(X_train, attributes, pattern, 0.005, 12)
+predictor = Classifier.get_predictor(X_train, attributes, pattern, 0.005, 10)
 
 prediction = predictor(X_test)
 df_aux = np.where(prediction == y_test, 1, 0)
